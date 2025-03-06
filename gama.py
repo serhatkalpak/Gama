634f3bd4a65e7469deb4723fec23770f408d0dae
serhatkalpak <101672737+serhatkalpak@users.noreply.github.com>
2025-03-07 00:49:17 +0300

    Update gama.py

--- gama.py
+++ gama.py
@@ -19,9 +19,6 @@ def main(stdscr):
     curses.init_pair(COLOR_FOOD, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(COLOR_SNAKE, curses.COLOR_GREEN, curses.COLOR_BLACK)
     
-    # Fare olaylarını etkinleştir
-    curses.mousemask(curses.ALL_MOUSE_EVENTS | curses.REPORT_MOUSE_POSITION)
-    
     snake = [[4, 4], [4, 3], [4, 2]]
     direction = KEY_RIGHT
     food = [randint(1, HEIGHT-2), randint(1, WIDTH-2)]
@@ -30,33 +27,17 @@ def main(stdscr):
     while True:
         key = stdscr.getch()
         
-        # Dokunma olaylarını kontrol et
-        if key == curses.KEY_MOUSE:
-            try:
-                _, mx, my, _, bstate = curses.getmouse()
-                if bstate & curses.BUTTON1_PRESSED:
-                    head_y, head_x = snake[0][0], snake[0][1]
-                    
-                    # Dokunulan yöne göre hareket
-                    dx = mx - head_x
-                    dy = my - head_y
-                    
-                    if abs(dx) > abs(dy):
-                        new_dir = KEY_RIGHT if dx > 0 else KEY_LEFT
-                    else:
-                        new_dir = KEY_DOWN if dy > 0 else KEY_UP
-                    
-                    # Ters yöne engel
-                    if (direction, new_dir) not in [(KEY_LEFT, KEY_RIGHT), 
-                                                   (KEY_RIGHT, KEY_LEFT),
-                                                   (KEY_UP, KEY_DOWN),
-                                                   (KEY_DOWN, KEY_UP)]:
-                        direction = new_dir
-            except:
-                pass
+        # Yön tuşları kontrolü
+        if key in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN]:
+            # Yılanın ters yöne gitmesini engelle
+            if (key == KEY_LEFT and direction != KEY_RIGHT) or \
+               (key == KEY_RIGHT and direction != KEY_LEFT) or \
+               (key == KEY_UP and direction != KEY_DOWN) or \
+               (key == KEY_DOWN and direction != KEY_UP):
+                direction = key
         
         # Yılan hareketi
-        head = [snake[0][0], snake[0][1]]
+        head = [snake[0][0], snake[0][1]
         if direction == KEY_UP: head[0] -= 1
         elif direction == KEY_DOWN: head[0] += 1
         elif direction == KEY_LEFT: head[1] -= 1
@@ -68,7 +49,7 @@ def main(stdscr):
             head in snake):
             break
             
-        snake.insert(0, head)
+        snake.insert(0, list(head))
         
         # Yem yeme
         if head == food:
